509bc991e945b12f86b1f16ff67e0807136df27c
Fajar Tri Saputra <fajartrisaputra@gmail.com>
2020-02-11 14:25:27 +0700

    update component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { AppComponent } from './app.component';
 import { FaqComponent} from './faq/faq.component';
 import { HomeComponent} from './home/home.component';
 import { AboutUsComponent} from './about-us/about-us.component';
